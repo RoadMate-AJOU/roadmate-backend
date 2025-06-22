8b0b18f13e27044ec3b048efc6c02c85f44800a9
1-J-1 <nowijnah15@gmail.com>
2025-06-23 01:21:13 +0900

    🚧 chore : 초기 설정

---
+++ src/test/java/ajou/roadmate/RoadmateApplicationTests.java
@@ -0,0 +1,13 @@
+package ajou.roadmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadmateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
