89919015729f2cfaff180b2b42e9624bafcb36bc
1-J-1 <nowijnah15@gmail.com>
2025-07-19 13:08:25 +0900

    🎉 update : redis 및 error code 수정
    - user 관련 레디스 설정 파일 수정
    - error code 추가
    
    Related to #9

---
+++ src/main/java/ajou/roadmate/global/config/RedisConfig.java
@@ -0,0 +1,32 @@
+package ajou.roadmate.global.config;
+
+import ajou.roadmate.gpt.dto.ChatContext;
+import ajou.roadmate.user.domain.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, ChatContext> chatContextRedisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, ChatContext> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
+    }
+
+    @Bean
+    public RedisTemplate<String, User> userRedisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, User> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
+    }
+}
